6d9622746d143807a2c1adbc027a3bf4a131f376
WilliamBendtsen <eaa23wfb@students.eaaa.dk>
2024-12-02 10:07:28 +0100

    navbar fix

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,6 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <nav>
-        <li>CUPHEAD</li>
-        <li>STORE</li>
-        <li>CUPHEAD-VERSE</li>
-        <li>BEHIND THE SCENES</li>
-      </nav>
       <main></main>
     </div>
   );
