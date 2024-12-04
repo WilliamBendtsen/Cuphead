ec1399e12e6edb7bee7d1e4f2c23059f971193b8
RuneGSoerensen <143501521+RuneGSoerensen@users.noreply.github.com>
2024-12-04 11:00:34 +0100

    cuphead-static-done

--- app/components/Baroness.tsx
+++ app/components/Baroness.tsx
@@ -8,7 +8,7 @@ export default function Baroness() {
              src={BaronessImage} alt="Evil Baroness" width={400} height={400} 
              className="w-full h-auto"
              />
-             <p className="absolute w-1/2 text-4xl text-white bg-green-950 bg-opacity-30 p-5 rounded-3xl top-50 right-12">Baroness Von Bon Bon is the eccentric candy queen of Sugarland in the Cuphead-verse, 
+             <p className="absolute w-1/2 text-4xl text-white bg-slate-800 bg-opacity-30 p-5 rounded-3xl top-50 left-12">Baroness Von Bon Bon is the eccentric candy queen of Sugarland in the Cuphead-verse, 
              ruling her sugary domain with a mix of charm and menace. As a boss in the Sugarland Shimmy level, she commands a variety of candy-themed minions and uses
               her confectionery powers to challenge players, embodying both the whimsical and perilous nature of Inkwell Isle Two.</p>
         </div>
