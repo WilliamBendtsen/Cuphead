9d533d065a453d0f04407ea2dfecb115865a9b61
RuneGSoerensen <143501521+RuneGSoerensen@users.noreply.github.com>
2024-12-04 10:51:28 +0100

    static

--- app/components/CupheadMugman.tsx
+++ app/components/CupheadMugman.tsx
@@ -8,7 +8,10 @@ export default function CupheadMugmanImage() {
              src={cupheadMugmanImage} alt="Cuphead and Mugman" width={400} height={400} 
              className="w-full h-auto"
              />
-             <p className="absolute w-1/2 text-4xl text-white bg-green-950 bg-opacity-30 p-5 rounded-3xl top-50 right-12">Cuphead and mug man are Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deserunt, cumque natus! Dolor adipisci quo illo amet molestiae, nemo consequatur atque. Architecto explicabo enim quia nihil quibusdam maiores cupiditate illo at.</p>
+             <p className="absolute w-1/2 text-4xl text-white bg-green-950 bg-opacity-30 p-5 rounded-3xl top-50 right-12">Cuphead and Mugman are two adventurous brothers from
+              the whimsical world of the Cuphead-verse, known for their contrasting personalities—Cuphead, the impulsive risk-taker, and Mugman,
+               the cautious voice of reason. Together, they reside on Inkwell Isle under Elder Kettle's care and embark on a high-stakes journey to repay a 
+               debt to the Devil, battling challenging foes and showcasing themes of teamwork, bravery, and brotherhood.</p>
         </div>
     )
 }
