60d564b15fe1857175ba6742a6c20e2d765a7469
WilliamBendtsen <143700870+WilliamBendtsen@users.noreply.github.com>
2024-12-05 11:31:14 +0100

    landing page 1

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -1,9 +1,17 @@
 import Link from "next/link";
+import Image from "next/image";
 
 export default function Nav() {
   return (
-    <nav className="top-0 left-0 w-full z-50 flex justify-around items-center p-4">
-      <Link href="/">HOME</Link>
+    <nav className="top-0 left-0 w-full flex justify-around items-center p-4">
+      <Link href="/">
+        <Image
+          src="/img/cuphead-logo.png"
+          alt="Cuphead Logo"
+          width={200}
+          height={200}
+        />
+      </Link>
       <Link href="/store">STORE</Link>
       <Link href="/cuphead-verse">CUPHEAD-VERSE</Link>
       <Link href="/behind-the-scenes">BEHIND THE SCENES</Link>
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,30 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
     <div>
-      <main></main>
+      <main className="grid grid-cols-12 grid-rows-12 h-screen">
+        {/* DLC Title Text */}
+        <h1 className="text-4xl col-start-5 col-span-4 row-start-3 justify-self-center font-extrabold">
+          Meet Ms Chalice in the new DLC!
+        </h1>
+        {/* DLC Image */}
+        <Image
+          className="col-start-5 col-span-4 row-start-4 row-span-7 justify-self-center"
+          src="/img/ms-chalice-dlc-poster.png"
+          alt="Ms Chalice DLC Poster"
+          height={400}
+          width={400}
+        />
+
+        {/* Buy Now Button */}
+        <button className="w-3/6 h-2/5 bg-purple-800 hover:bg-purple-950  text-white font-extrabold rounded col-start-6 col-span-2 row-start-8 row-span-2 self-center  justify-self-center">
+          BUY NOW
+        </button>
+
+        {/* Sheet Music Div */}
+        <div></div>
+      </main>
     </div>
   );
 }
