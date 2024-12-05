5b50112d95e5c5b3ddd5b1e26423105e6796d1cb
WilliamBendtsen <143700870+WilliamBendtsen@users.noreply.github.com>
2024-12-05 10:27:38 +0100

    store 1920x1080

--- app/store/page.tsx
+++ app/store/page.tsx
@@ -16,30 +16,39 @@ export default function Store() {
 
         {/* Porkrind Shop Table */}
         <Image
-          className="relative z-30"
+          className="absolute z-30"
           src="/img/porkrind-table.png"
           alt="Porkrind Shop Table"
-          width={1200}
-          height={200}
+          style={{
+            width: "70%",
+          }}
+          width={2000}
+          height={2000}
         />
 
         {/* Porkrind Character Model */}
         <Image
-          className="absolute bottom-[36%] right-[12%] z-40"
+          className="absolute bottom-[36.5%] right-[19%] z-40"
           src="/img/porkrind.png"
           alt="Porkrind"
-          width={400}
-          height={400}
+          style={{
+            width: "20%",
+          }}
+          width={1000}
+          height={1000}
         />
 
         {/* Mugman Marching Character Model +  Sheet Music Popup */}
         <div className="group">
           <Image
-            className="absolute bottom-[8%] left-[15%] z-10 transition-all group-hover:z-20"
+            className="absolute bottom-[7.5%] left-[20%] z-10 transition-all group-hover:z-20"
             src="/img/porkrind-sheet-music-picture.png"
             alt="Sheet Music Picture"
-            width={500}
-            height={400}
+            style={{
+              width: "28%",
+            }}
+            width={1000}
+            height={1000}
           />
 
           <Link
@@ -47,7 +56,7 @@ export default function Store() {
             className="group"
           >
             <Image
-              className="absolute bottom-1/3 right-[40%] z-40 group-hover:scale-110 transform transition-transform duration-500 ease-in-out hover:-translate-y-1/4 group-hover:drop-shadow-glow"
+              className="absolute bottom-1/3 left-[48%] z-40 group-hover:scale-110 transform transition-transform duration-500 ease-in-out hover:-translate-y-1/4 group-hover:drop-shadow-glow"
               src="/img/mugman-marching.png"
               alt="Mugman Marching"
               width={200}
@@ -59,11 +68,14 @@ export default function Store() {
         {/* Ms. Chalice Character Model +  DLC Popup */}
         <div className="group">
           <Image
-            className="absolute bottom-[8%] left-[15%] z-10 transition-all group-hover:z-20"
+            className="absolute bottom-[7.5%] left-[20%] z-10 transition-all group-hover:z-20"
             src="/img/porkrind-dlc-picture.png"
             alt="DLC Picture"
-            width={500}
-            height={400}
+            style={{
+              width: "28%",
+            }}
+            width={1000}
+            height={1000}
           />
 
           <Link
@@ -71,7 +83,7 @@ export default function Store() {
             className="group"
           >
             <Image
-              className="absolute bottom-1/3 pb-[1%] left-[33%] z-40 group-hover:scale-110 transform transition-transform duration-500 ease-in-out hover:-translate-y-1/4 group-hover:drop-shadow-glow"
+              className="absolute bottom-1/3 pb-[1%] left-[37%] z-40 group-hover:scale-110 transform transition-transform duration-500 ease-in-out hover:-translate-y-1/4 group-hover:drop-shadow-glow"
               src="/img/ms-chalice.png"
               alt="Ms. Chalice"
               width={150}
@@ -83,18 +95,21 @@ export default function Store() {
         {/* Cuphead & Mugman Character Models + Main Game Popup */}
         <div className="group">
           <Image
-            className="absolute bottom-[8%] left-[15%] z-10 transition-all group-hover:z-20"
+            className="absolute bottom-[7.5%] left-[20%] z-10 transition-all group-hover:z-20"
             src="/img/porkrind-main-game-picture.png"
             alt="Main Game Picture"
-            width={500}
-            height={400}
+            style={{
+              width: "28%",
+            }}
+            width={1000}
+            height={1000}
           />
           <Link
             href="https://www.nintendo.com/us/store/products/cuphead-switch/"
             className="group"
           >
             <Image
-              className="absolute bottom-1/3 pb-5 left-[15%] z-40 group-hover:scale-110 transform transition-transform duration-500 ease-in-out hover:-translate-y-1/4 group-hover:drop-shadow-glow"
+              className="absolute bottom-1/3 pb-5 left-[20%] z-40 group-hover:scale-110 transform transition-transform duration-500 ease-in-out hover:-translate-y-1/4 group-hover:drop-shadow-glow"
               src="/img/cuphead-&-mugman.png"
               alt="Cuphead & Mugman"
               width={200}
@@ -105,11 +120,14 @@ export default function Store() {
 
         {/* "Choose Your Character" Image */}
         <Image
-          className="absolute bottom-[8%] left-[15%] z-10"
+          className="absolute bottom-[7.5%] left-[20%] z-10"
           src="/img/choose-your-character.png"
           alt="Choose Your Character"
-          width={500}
-          height={400}
+          style={{
+            width: "28%",
+          }}
+          width={1000}
+          height={1000}
         />
       </div>
     </div>
