78cf8aa349b7e1645b39b1baacf224e4c62c53d4
RuneGSoerensen <eaa23rgso@students.eaaa.dk>
2024-12-13 13:04:39 +0100

    Metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,28 @@
 import "./globals.css";
 import Nav from "./components/Nav";
 import Footer from "./components/Footer";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Fanmade School Cuphead project",
+  description:
+    "This is a school exam project about Cuphead. We hold no rights to Cuphead; this is purely for educational purposes and developer practice.",
+  metadataBase: new URL("https://cuphead-mmd.vercel.app"),
+  keywords: [
+    "Cuphead",
+    "fan project",
+    "school project",
+    "game development",
+    "frontend development",
+  ],
+  authors: [
+    { name: "Rune Gerner Søresen", url: "https://runegstudio.dk/" },
+    {
+      name: "William Frøjk Bendtsen",
+      url: "https://portfolie.williamskodeeventyr.dk/",
+    },
+  ],
+};
 
 export default function RootLayout({
   children,
