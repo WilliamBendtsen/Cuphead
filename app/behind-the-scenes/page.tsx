98b5aa387b12ba5ab4702e86c0d840f73b72c585
RuneGSoerensen <eaa23rgso@students.eaaa.dk>
2024-12-13 12:47:32 +0100

    deployment test

--- app/behind-the-scenes/page.tsx
+++ app/behind-the-scenes/page.tsx
@@ -6,13 +6,11 @@ const response = await fetch(
 const bts = await response.json();
 
 export default function BehindTheScenes() {
-  
-
   return (
     <>
       <div className="my-9 text-center flex items-center flex-col justify-center mx-auto w-full lg:w-1/2">
         <h1 className="text-6xl pb-3 text-zinc-800">
-          Explore the Making of Cuphead
+          Explore the Making of Last of us 2
         </h1>
         <p className="text-lg text-center py-2 text-zinc-800">
           Discover the artistry and dedication that brought Cuphead to life.
@@ -23,9 +21,15 @@ export default function BehindTheScenes() {
       </div>
       <div className="flex items-center justify-center flex-wrap sm:flex-col xl:flex-row p-5">
         <section className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-          {bts.map((behind: { id: number; acf: { btstitle: string; btsvideourl: string; btsdesc: string }; title: { rendered: string } }) => (
-            <BehindTheScenesCard key={behind.id} behind={behind} />
-          ))}
+          {bts.map(
+            (behind: {
+              id: number;
+              acf: { btstitle: string; btsvideourl: string; btsdesc: string };
+              title: { rendered: string };
+            }) => (
+              <BehindTheScenesCard key={behind.id} behind={behind} />
+            )
+          )}
         </section>
       </div>
     </>
