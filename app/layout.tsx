ac07d0e9f2a7ad8cb346bff312d0da5553317f99
RuneGSoerensen <eaa23rgso@students.eaaa.dk>
2024-12-17 11:03:27 +0100

    metaupdate

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import Footer from "./components/Footer";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Fanmade School Cuphead project",
+  title: "Cuphead Fanmade School project",
   description:
     "This is a school exam project about Cuphead. We hold no rights to Cuphead; this is purely forS educational purposes and developer practice.",
   metadataBase: new URL("https://cuphead-mmd.vercel.app"),
